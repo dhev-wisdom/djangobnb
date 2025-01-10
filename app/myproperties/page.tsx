22fa049a0a640c85e9689c3370c62d6dbee17b55
dhev-wisdom <nonsowisdom62@gmail.com>
2025-01-09 18:30:36 -0600

    finished my properties page skeleton

--- app/myproperties/page.tsx
+++ app/myproperties/page.tsx
@@ -6,89 +6,10 @@ const MyPropertiesPage = () => {
     return (
         <main className="max-w-1500pxl mx-auto px-6 pb-6">
             <div className="pt-6 pb-2 ">
-                <h1 className="my-6 text-2xl">My MyPropertiesPage Page</h1>
+                <h1 className="my-6 text-2xl">My Properties Page</h1>
 
-                <div className="space-y-4">
-                    <div className="p-5 rounded-xl grid grid-cols-1 md:grid-cols-4 shadow-md gap-4 border border-gray-300">
-                        <div className="col-span-1">
-                            <div className="relative overflow-hidden aspect-square rounded-xl">
-                                <Image 
-                                    fill
-                                    src="/beach.jpg"
-                                    className="hover:scale-110 object-cover transition w-full h-full"
-                                    alt="Beach house"
-                                />
-                            </div>
-                        </div>
-
-                        <div className="col-span-1 md:col-span-3">
-                            <h2 className="mb-4 text-xl">Property Name</h2>
-
-                            <p className="mb-2"><strong>Check-in date:</strong>  14/12/2024</p>
-                            <p className="mb-2"><strong>Check-out date:</strong>  21/12/2024</p>
-
-                            <p className="mb-2"><strong>Number of nights:</strong>  7</p>
-                            <p className="mb-2"><strong>Total price:</strong>  $820</p>
-
-                            <div className="mt-6 bg-airbnb hover:bg-airbnb-dark text-white cursor-pointer inline-block p-4 rounded-xl">
-                                Go to property
-                            </div>
-                        </div>
-                    </div>
-
-                    <div className="p-5 rounded-xl grid grid-cols-1 md:grid-cols-4 shadow-md gap-4 border border-gray-300">
-                        <div className="col-span-1">
-                            <div className="relative overflow-hidden aspect-square rounded-xl">
-                                <Image 
-                                    fill
-                                    src="/beach.jpg"
-                                    className="hover:scale-110 object-cover transition w-full h-full"
-                                    alt="Beach house"
-                                />
-                            </div>
-                        </div>
-
-                        <div className="col-span-1 md:col-span-3">
-                            <h2 className="mb-4 text-xl">Property Name</h2>
-
-                            <p className="mb-2"><strong>Check-in date:</strong>  14/12/2024</p>
-                            <p className="mb-2"><strong>Check-out date:</strong>  21/12/2024</p>
-
-                            <p className="mb-2"><strong>Number of nights:</strong>  7</p>
-                            <p className="mb-2"><strong>Total price:</strong>  $820</p>
-
-                            <div className="mt-6 bg-airbnb hover:bg-airbnb-dark text-white cursor-pointer inline-block p-4 rounded-xl">
-                                Go to property
-                            </div>
-                        </div>
-                    </div>
-
-                    <div className="p-5 rounded-xl grid grid-cols-1 md:grid-cols-4 shadow-md gap-4 border border-gray-300">
-                        <div className="col-span-1">
-                            <div className="relative overflow-hidden aspect-square rounded-xl">
-                                <Image 
-                                    fill
-                                    src="/beach.jpg"
-                                    className="hover:scale-110 object-cover transition w-full h-full"
-                                    alt="Beach house"
-                                />
-                            </div>
-                        </div>
-
-                        <div className="col-span-1 md:col-span-3">
-                            <h2 className="mb-4 text-xl">Property Name</h2>
-
-                            <p className="mb-2"><strong>Check-in date:</strong>  14/12/2024</p>
-                            <p className="mb-2"><strong>Check-out date:</strong>  21/12/2024</p>
-
-                            <p className="mb-2"><strong>Number of nights:</strong>  7</p>
-                            <p className="mb-2"><strong>Total price:</strong>  $820</p>
-
-                            <div className="mt-6 bg-airbnb hover:bg-airbnb-dark text-white cursor-pointer inline-block p-4 rounded-xl">
-                                Go to property
-                            </div>
-                        </div>
-                    </div>
+                <div className="grid grid-cols-1 md:grid-cols-3 lg:grid-cols-3 gap-4 transition">
+                    <PropertyList />
                 </div>
             </div>
         </main>
